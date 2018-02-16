c3b3a78f0c07546e68d3f2d747e82ea875d8fbca
jacquelynmarcella <jacquelyn.marcella@gmail.com>
2018-02-16 09:37:32 -0800

    Working on logic for rendering team selections

--- client/src/game/Create.js
+++ client/src/game/Create.js
@@ -2,13 +2,68 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
+class InfluencerOption extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      selected: false
+    }
+  }
+
+  //On click, add to data
+  handleClick = (event) => {
+    event.preventDefault();
+    console.log("event target id", event.target.id);
+    this.props.handleSelect(event.target.id);
+    if (this.state.selected === false){
+      this.setState({
+        selected: true
+      })
+    }
+    else {
+      this.setState({
+        selected: false
+      })
+    } 
+    console.log(this.state);
+  }
+
+  render(){
+    var selectedClass;
+    if (this.state.selected) {
+      selectedClass = "influencerOption selected"
+    }
+    else {
+      selectedClass = "influencerOption unselected"
+    }
+    return(
+      <a href={this.props.id} alt="Select influencerOption" onClick={this.handleClick} id={this.props.id}>
+      <div className={selectedClass}>
+        <div className="influencerImg">
+          <img src="https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?w=940&h=650&auto=compress&cs=tinysrgb" />
+        </div>
+        <div className="influencerContent">
+          <h5>Name</h5>
+          <p>Stats</p>
+          <p>Stats</p>
+        </div>
+      </div>
+      </a>
+    )
+
+  }
+}
+
+
 class Create extends Component {
   constructor(props){
     super(props);
     this.state = {
       name: '',
       startDate: '',
-      inviteKey: ''
+      inviteKey: '',
+      creationStage: 'step1',
+      selectedList: []
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -23,9 +78,20 @@ class Create extends Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
-    console.log("Axios call for creating new league; this may exist in another component but need to pass the data through here")
+    if(this.state.creationStage === "step1") {
+      this.setState({
+        creationStage: "step2"
+      })
+    }
+    else if(this.state.creationStage === "step2") {
+      console.log("Axios call for creating new league; this may exist in another component but need to pass the data through here")
+    }
   }
 
+  handleSelect = (data) => {
+    console.log(data);
+  }
+    
   componentDidMount = () => {
     //Random generator for short shareable key
     let randomKey = Math.random().toString(36).replace(/[^A-Za-z0-9]+/g, '').substr(0, 5).toUpperCase();
@@ -35,23 +101,55 @@ class Create extends Component {
   }
 
   render(){
+    var display; 
+    if(this.state.creationStage === "step1") {
+      display = (
+        <div>
+          <h1>Create Your League</h1>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              League Name
+              <input name="name" placeholder="Name" type="text" onChange={this.handleChange} value={this.state.name} required />
+            </label>
+            <label>
+              Start Date
+              <input name="startDate" type="date" onChange={this.handleChange} required />
+            </label>
+          </form>
+          <input type="button" value="Invite Friends" disabled />
+          <input type="submit" value="Create League" onClick={this.handleSubmit} />
+          <h3>Copy to invite friends:</h3>
+          <p>{this.state.inviteKey}</p>
+        </div>
+      );
+    }
+    else if(this.state.creationStage === "step2") {
+        display = (
+          <div>
+            <h1>Pick your team</h1>
+            <InfluencerOption handleSelect={this.handleSelect} id="1" />
+            <InfluencerOption handleSelect={this.handleSelect} id="2" />
+            <InfluencerOption handleSelect={this.handleSelect} id="3" />
+            <InfluencerOption handleSelect={this.handleSelect} id="4" />
+            <InfluencerOption handleSelect={this.handleSelect} id="5" />
+            <InfluencerOption handleSelect={this.handleSelect} id="6" />
+            <InfluencerOption handleSelect={this.handleSelect} id="7" />
+            <InfluencerOption handleSelect={this.handleSelect} id="8" />
+            <InfluencerOption handleSelect={this.handleSelect} id="9" />
+            <InfluencerOption handleSelect={this.handleSelect} id="10" />
+            <InfluencerOption handleSelect={this.handleSelect} id="11" />
+            <InfluencerOption handleSelect={this.handleSelect} id="12" />
+            <br />
+            <center>
+             <input type="submit" value="Create League" onClick={this.handleSubmit} />
+            </center>
+          </div>
+        );
+    }
+
     return(
      <div>
-      <h1>Create Your League</h1>
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          League Name
-          <input name="name" placeholder="Name" type="text" onChange={this.handleChange} value={this.state.name} required />
-          </label>
-          <label>
-          Start Date
-          <input name="startDate" type="date" onChange={this.handleChange} required />
-          </label>
-        </form>
-        <input type="button" value="Invite Friends" disabled />
-        <input type="submit" value="Create League" onClick={this.handleSubmit} />
-        <h3>Copy to invite friends:</h3>
-        <p>{this.state.inviteKey}</p>
+        {display}
      </div>
 
       );
