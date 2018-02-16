074b0776fdc918c29150b1e5db85e2c615a18059
jacquelynmarcella <jacquelyn.marcella@gmail.com>
2018-02-16 14:47:01 -0800

    updated stats

--- client/src/game/Games.js
+++ client/src/game/Games.js
@@ -22,6 +22,24 @@ class Games extends Component {
 		          <p className="increase">+.01%</p>
 		        </div>
 	      	</div>
+	      	<div className="influencerOption">
+		      <div className="influencerImg">
+		          <img src="https://scontent-sea1-1.cdninstagram.com/vp/ddf8c9030040b733a82d2314bb86abcf/5B17BCF0/t51.2885-19/s320x320/24328051_136879256974724_970975059176849408_n.jpg" />
+		        </div>
+		        <div className="influencerContent">
+		          <h5>Ariana Grande</h5>
+		          <p className="increase">+.01%</p>
+		        </div>
+	      	</div>
+	      	<div className="influencerOption">
+		      <div className="influencerImg">
+		          <img src="https://scontent-sea1-1.cdninstagram.com/vp/ecfa173305130003e02faf666a57fab3/5B083521/t51.2885-19/s320x320/19228783_1421845407904949_3402248722799656960_a.jpg" />
+		        </div>
+		        <div className="influencerContent">
+		          <h5>Kim Kardashian</h5>
+		          <p className="increase">+.01%</p>
+		        </div>
+	      	</div>
 	 
 
 	      	<h4>Jim Bob</h4>
@@ -66,7 +84,25 @@ class Games extends Component {
 		        </div>
 		        <div className="influencerContent">
 		          <h5>The Rock</h5>
-		          <p className="increase">+.08%</p>
+		          <p className="increase">+.02%</p>
+		        </div>
+	      	</div>
+	      	<div className="influencerOption">
+		      <div className="influencerImg">
+		          <img src="https://scontent-sea1-1.cdninstagram.com/vp/ddf8c9030040b733a82d2314bb86abcf/5B17BCF0/t51.2885-19/s320x320/24328051_136879256974724_970975059176849408_n.jpg" />
+		        </div>
+		        <div className="influencerContent">
+		          <h5>Ariana Grande</h5>
+		          <p className="increase">+.01%</p>
+		        </div>
+	      	</div>
+	      	<div className="influencerOption">
+		      <div className="influencerImg">
+		          <img src="https://scontent-sea1-1.cdninstagram.com/vp/86357df6c18e6ed59d52ed29691fceb0/5B0F73D2/t51.2885-19/s320x320/26071962_172697516821900_8681115210095788032_n.jpg" />
+		        </div>
+		        <div className="influencerContent">
+		          <h5>Selena Gomez</h5>
+		          <p className="increase">+.01%</p>
 		        </div>
 	      	</div>
 	      	 
@@ -87,6 +123,15 @@ class Games extends Component {
 		        <div className="influencerContent">
 		          <h5>Kim Kardashian</h5>
 		          <p className="decrease">-.03%</p>
+		        </div>
+	      	</div>
+	      	  <div className="influencerOption" id={this.props.id}>
+		        <div className="influencerImg">
+		          <img src="https://scontent-sea1-1.cdninstagram.com/vp/95e861255d8f8dc6daa9a1891009fdba/5B20704F/t51.2885-19/s320x320/20969376_112654676087652_1378856425261891584_a.jpg" />
+		        </div>
+		        <div className="influencerContent">
+		          <h5>Taylor Swift</h5>
+		          <p className="increase">+.03%</p>
 		        </div>
 	      	</div>
 
